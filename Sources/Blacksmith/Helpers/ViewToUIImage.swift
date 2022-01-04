59d7d4f08c66e7be67e4c721eabedfaffa1319bf
Florian Schweizer <post.an.flo@gmail.com>
2022-01-04 19:17:12 +0100

    Try different snapshot function

--- Sources/Blacksmith/Helpers/ViewToUIImage.swift
+++ Sources/Blacksmith/Helpers/ViewToUIImage.swift
@@ -9,19 +9,23 @@ import SwiftUI
 #if canImport(UIKit)
 import UIKit
 
+import SwiftUI
+
 extension View {
     func snapshot(withSize size: CGSize) -> UIImage {
-        let controller = UIHostingController(rootView: self)
-        let view = controller.view
+        let controller = UIHostingController(rootView: self.frame(width: size.width, height: size.height))
+        controller.view.bounds = CGRect(origin: .zero, size: size)
+        let image = controller.view.asImage()
+        
+        return image
+    }
+}
 
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: size)
-        view?.backgroundColor = .clear
-
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+extension UIView {
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: self.bounds.size)
+        return renderer.image { ctx in
+            self.drawHierarchy(in: self.bounds, afterScreenUpdates: true)
         }
     }
 }
