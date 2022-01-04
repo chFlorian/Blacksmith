17b30989d5d05463b3b32f939395493270ceb0be
Florian Schweizer <post.an.flo@gmail.com>
2022-01-04 18:02:37 +0100

    Add custom ExportSize

--- Sources/Blacksmith/Helpers/ExportSize.swift
+++ Sources/Blacksmith/Helpers/ExportSize.swift
@@ -10,6 +10,7 @@ import SwiftUI
 public enum ExportSize {
     case iPhone
     case mac
+    case custom(CGSize, Double)
     
     public var size: CGSize {
         switch self {
@@ -17,6 +18,8 @@ public enum ExportSize {
                 return CGSize(width: 1242, height: 2688)
             case .mac:
                 return CGSize(width: 1280, height: 800)
+            case .custom(let size, _):
+                return size
         }
     }
     
@@ -26,6 +29,8 @@ public enum ExportSize {
                 return 40
             case .mac:
                 return 8
+            case .custom(_, let radius):
+                return radius
         }
     }
 }
