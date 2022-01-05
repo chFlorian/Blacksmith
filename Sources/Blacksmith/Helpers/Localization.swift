4e00c837fc07f8d0290ae75d866dc5d9e1e35c45
Florian Schweizer <post.an.flo@gmail.com>
2022-01-05 11:28:15 +0100

    Add Localization struct

---
+++ Sources/Blacksmith/Helpers/Localization.swift
@@ -0,0 +1,14 @@
+//
+//  Localization.swift
+//  
+//
+//  Created by Florian Schweizer on 05.01.22.
+//
+
+import Foundation
+
+struct Localization: Identifiable {
+    let id = UUID()
+    let locale: String
+    let title: String
+}
